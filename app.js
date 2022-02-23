2a29c501c76f665a05759f57c7a0323a54518065
frxncismor <FMoreno1@gannet.com>
2022-02-23 15:09:51 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+function generate() {
+  let input = document.querySelector('#input').value;
+  let lines = input.split('\n');
+  let output = document.querySelector("#output");
+  let outputArray = [];
+
+  for(const line of lines) {
+    if (/\S/.test(line)) {
+      if (!line.includes('#')) {
+				splitted = line.split(' ');
+				origin = splitted[0];
+				destination = splitted[1];
+				indexOfDomain = origin.indexOf('.');
+				cleanOrigin = origin.slice(indexOfDomain + 1);
+				indexofTopLevelDomain = cleanOrigin.indexOf('.');
+				siteChanged = cleanOrigin.slice(0, indexofTopLevelDomain);
+        prDescription = `
+        ### ${siteChanged}			
+        - Production: https://www.${cleanOrigin}
+        - CI: https://ci-www.${cleanOrigin}
+        - Staging: https://staging-www.${cleanOrigin}
+        - Destination: ${destination}
+        
+        `;
+        outputArray.push(prDescription);
+      }
+    }
+  }
+  console.log(outputArray);
+  output.innerText = outputArray.join('');
+
+}
